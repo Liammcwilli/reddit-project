849628380f1634ab2cc5889b924772a66cf1ccb7
Liam McWilliams <liamthemcwilliams@gmail.com>
2023-09-26 16:11:30 -0400

    fix error

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import Navbar from '../Navbar/Navbar';
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../firebase/clientApp";
+import Navbar from "../Navbar/Navbar";
 
-const Layout:React.FC = ({ children }) => {
-    
-    return (
-        <>
-        <Navbar />
-        <main>{children}</main>
-        </>
-    )
+type Props = {
+  children: React.ReactNode
 }
+
+
+const Layout:React.FC<Props> = ({ children }) => {
+  // useAuth(); // will implement later at end of tutorial
+
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
+};
+
 export default Layout;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,20 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
-import { ChakraProvider } from '@chakra-ui/react';
+import { RecoilRoot } from "recoil";
 import { theme } from "../chakra/theme";
 import Layout from "../components/Layout/Layout";
-import { RecoilRoot } from "recoil";
+import "../firebase/clientApp";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-      <RecoilRoot>
-        <ChakraProvider theme={theme}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </ChakraProvider>
-      </RecoilRoot>
-  )
+    <RecoilRoot>
+      <ChakraProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </RecoilRoot>
+  );
 }
 
 export default MyApp;
