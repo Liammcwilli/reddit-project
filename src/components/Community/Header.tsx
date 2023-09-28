1c48c47873ef7af97ac66778787ae58b20faa785
Liam McWilliams <liamthemcwilliams@gmail.com>
2023-09-28 17:53:38 -0400

    updated isjoined

--- src/components/Community/Header.tsx
+++ src/components/Community/Header.tsx
@@ -2,6 +2,7 @@ import { Community } from '@/atoms/communitiesAtom';
 import React from 'react'; 
 import { Flex, Box, Icon, Image, Text, Button } from '@chakra-ui/react'
 import { FaReddit } from 'react-icons/fa';
+import useCommunityData from '@/hooks/useCommunityData';
 
 type HeaderProps = {
     communityData: Community;
@@ -9,7 +10,10 @@ type HeaderProps = {
 
 const Header:React.FC<HeaderProps> = ({ communityData }) => {
 
-    const isJoined = false;
+
+    const { communityStateValue, onJoinOrLeaveCommunity, loading } = useCommunityData();
+    const isJoined = !!communityStateValue.mySnippets.find(
+        (item) => item.communityId === communityData.id)
     
     return (
     <Flex direction='column' width='100%' height='146px'>
@@ -39,7 +43,8 @@ const Header:React.FC<HeaderProps> = ({ communityData }) => {
                     height="30px"
                     pr={6}
                     pl={6}
-                    onClick={() => {}}
+                    onClick={() => onJoinOrLeaveCommunity(communityData, isJoined)}
+                    isLoading={loading}
                     
                     >
                         {isJoined ? 'Joined' : 'Join'}
