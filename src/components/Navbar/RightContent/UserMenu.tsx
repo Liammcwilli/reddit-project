de9785ffbe3ea066bed694bd686ac62906aec7e4
Liam McWilliams <liamthemcwilliams@gmail.com>
2023-09-28 17:55:40 -0400

    added  logout

--- src/components/Navbar/RightContent/UserMenu.tsx
+++ src/components/Navbar/RightContent/UserMenu.tsx
@@ -8,8 +8,9 @@ import { ChevronDownIcon } from '@chakra-ui/icons';
 import { CgProfile } from 'react-icons/cg';
 import {MdOutlineLogin} from 'react-icons/md';
 import { auth } from '@/firebase/clientApp';
-import { useSetRecoilState } from 'recoil';
+import { useResetRecoilState, useSetRecoilState } from 'recoil';
 import { AuthModalState } from '@/atoms/authModalAtom';
+import { communityState } from '@/atoms/communitiesAtom';
 
 type UserMenuProps = {
     user?: User | null;
@@ -17,7 +18,15 @@ type UserMenuProps = {
 };
 
 const UserMenu:React.FC<UserMenuProps> = ({ user }) => {
+    const resetCommunityState = useResetRecoilState(communityState);
+
     const setAuthModalState = useSetRecoilState(AuthModalState);
+
+
+    const logout = async () => {
+        await signOut(auth);
+        resetCommunityState();
+    }
     
     return (
         <Menu>
@@ -83,7 +92,7 @@ const UserMenu:React.FC<UserMenuProps> = ({ user }) => {
                     fontWeight={700}
                     color="black"
                     _hover={{ bg: 'blue.500', color: 'white'}}
-                    onClick={() => signOut(auth)}
+                    onClick={logout}
                     >
                     <Flex align="center">
                             <Icon as={MdOutlineLogin} fontSize={20} mr={2} />
