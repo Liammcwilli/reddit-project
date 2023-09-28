ff9e3d9c8a2c3303693fedc3949303b0d1a9be0b
Liam McWilliams <liamthemcwilliams@gmail.com>
2023-09-28 17:53:23 -0400

    worked on post link

--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -1,4 +1,5 @@
 import { Community } from '@/atoms/communitiesAtom';
+import CreatePostLink from '@/components/Community/CreatePostLink';
 import Header from '@/components/Community/Header';
 import NotFound from '@/components/Community/NotFound';
 import PageContent from '@/components/Layout/PageContent';
@@ -24,7 +25,7 @@ const CommunityPage:React.FC<CommunityPageProps> = ({ communityData }) => {
     return <>
             <Header communityData={communityData} />
             <PageContent>
-                <><div>LHS</div></>
+                <><CreatePostLink /></>
                 <><div>RHS</div></>
             </PageContent>
             </>
