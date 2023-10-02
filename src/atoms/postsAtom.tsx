c50632ce87d034e9a898dec543e9a385710cc25f
Liam McWilliams <liamthemcwilliams@gmail.com>
2023-10-01 23:55:35 -0400

    finalized posts atom

--- src/atoms/postsAtom.tsx
+++ src/atoms/postsAtom.tsx
@@ -2,7 +2,7 @@ import { Timestamp } from 'firebase/firestore';
 import {atom } from 'recoil';
 
 export type Post = {
-    id: string;
+    id?: string;
     communityId: string;
     creatorId: string;
     creatorDisplayName: string;
@@ -16,15 +16,23 @@ export type Post = {
 
 }
 
+export type PostVote = {
+    id: string;
+    postId: string;
+    communityId: string;
+    voteValue: number;
+}
+
 interface PostState {
     selectedPost: Post | null;
     posts: Post[];
-    //post votes
+    postVotes: PostVote[],
 }
 
 const defaultPostState: PostState = {
     selectedPost: null,
     posts: [],
+    postVotes: [],
 }
 
 export const postState= atom<PostState>({
