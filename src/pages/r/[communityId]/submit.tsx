e03530369c8ac47a30b8880eeb01772e6076aa2a
Liam McWilliams <liamthemcwilliams@gmail.com>
2023-10-01 23:55:16 -0400

    updated the submit page to include about

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -4,11 +4,18 @@ import { Text, Box } from '@chakra-ui/react';
 import NewPostForm from '@/components/Posts/NewPostForm';
 import { auth } from '@/firebase/clientApp';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useRecoilValue } from 'recoil';
+import { communityState } from '@/atoms/communitiesAtom';
+import useCommunityData from '@/hooks/useCommunityData';
+import About from '@/components/Community/About';
 
 
 
 const submitPostPage:React.FC = () => {
     const [user] = useAuthState(auth);
+    // const communityStateValue = useRecoilValue(communityState);
+    const { communityStateValue } = useCommunityData();
+    console.log('COMMUNITY', communityStateValue);
     
     return (
         <PageContent>
@@ -18,7 +25,7 @@ const submitPostPage:React.FC = () => {
                 </Box>
                 {user && <NewPostForm user={user} />}
             </>
-
+                {communityStateValue.currentCommunity && <About communityData={communityStateValue.currentCommunity} />}
             <> </>
 
         </PageContent>
